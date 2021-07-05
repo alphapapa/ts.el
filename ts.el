f46717a04907581a6d51d45c4707013f17fda123
Adam Porter <adam@alphapapa.net>
2021-07-04 22:40:40 -0500

    Meta: 0.3-pre

--- ts.el
+++ ts.el
@@ -4,7 +4,7 @@
 
 ;; Author: Adam Porter <adam@alphapapa.net
 ;; URL: http://github.com/alphapapa/ts.el
-;; Version: 0.2
+;; Version: 0.3-pre
 ;; Package-Requires: ((emacs "26.1") (dash "2.14.1") (s "1.12.0"))
 ;; Keywords: calendar, lisp
 
